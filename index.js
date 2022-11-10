3aa2d0f84da4ad872e1fbed199872bd3009a40d4
Mostafizur Rahman <mostafizur0195@gmail.com>
2022-11-10 11:50:42 +0600

    Vercel Hosting test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -14,8 +15,7 @@ app.use(express.json());
 // process.env.DB_USER
 // process.env.DB_PASSWORD
 
-const uri =
-  "mongodb+srv://travelGuru:1FhsnUyK8wHosOWV@cluster0.5xecsyp.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5xecsyp.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
